69877f160987d2eec60b365ac511a17a3903d926
newtondotcom <asphalt9fr@gmail.com>
2023-10-05 22:11:48 +0200

    grid for last section

--- src/App.svelte
+++ src/App.svelte
@@ -23,6 +23,9 @@
   };
   
   onMount(() => {
+    
+    preloadImages();
+
     html = document.documentElement;
   
     canvas = document.getElementById("hero-lightpass");
@@ -33,7 +36,6 @@
     isDisplaying = true;
   
     context = canvas.getContext("2d");
-    preloadImages();
     img = new Image();
     img.src = currentFrame(1);
     canvas.width = window.innerWidth;
@@ -54,7 +56,6 @@
       if (isDisplaying) {
         requestAnimationFrame(() => updateImage(frameIndex + 1))
       }
-      console.log(maxScrollDown);
       if (maxScrollDown < scrollTop) {
         canvas.style.position = 'relative';
         canvas.style.top = '40%';
@@ -77,6 +78,7 @@
       const img = new Image();
       img.src = currentFrame(i);
     }
+    console.log('preloadImages');
   };
 
   function setmaxScrollDown() {
@@ -91,9 +93,6 @@
     maxScrollDown = sectionRect.bottom + 1.5*window.outerHeight - screenHeight + threshold;
   }
 }
-
-
-
   </script>
   
   <section class="hero">
@@ -104,7 +103,7 @@
   </section>
   
   <section class="about">
-    <div class="container">
+    <div class="container left">
       <h1>FOODSTOCK</h1>
       <h2>Scroll down</h2>
       <h2>Scroll down</h2>
@@ -114,11 +113,27 @@
   
   <section class="canvas">
     <canvas id="hero-lightpass" />
+    <div class="container second">
+      <h1>FOODSTOCK</h1>
+      <h2>Scroll down</h2>
+      <h2>Scroll down</h2>
+      <h2>Scroll down</h2>
+  </div>
   </section>
 
   <section class="try">
-    <div class="container">
-      <button>try it</button>
+    <div class="grid">
+      <div class="lt">You want to try it ?</div>
+      <div class="lm"></div>
+      <div class="lb">Greetings from the foodstock team</div>
+      <div class="mb"></div>
+      <div class="mm">
+        <button on:click={()=>window.location.href="/"}>here</button>
+      </div>
+      <div class="mt"></div>
+      <div class="rt">a lot of people would thank you for using foodstock .. </div>
+      <div class="rm"></div>
+      <div class="rb">you should not be hesitating that much ..</div>
     </div>
   </section>
 
@@ -169,5 +184,66 @@ section {
 .try {
   background-color: red;
 }
+
+button {
+  background-color: transparent;
+  border: 2px solid white;
+  border-radius: 10px;
+  padding: 10px 20px;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: white;
+  color: black;
+}
+
+.left {
+  position: absolute;
+  right: 10%;
+}
+
+.second {
+  position: absolute;
+  left: 10%;
+}
+
+.grid {  
+  margin-top: 100px;
+  margin-left: 50px;
+  margin-right: 50px;
+  display: grid;
+  grid-template-columns: 2.5fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 2fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  width: 100vw;
+  height: 100vh;
+}
+
+.lt { grid-area: 1 / 1 / 2 / 2;
+font-size: 5vw; }
+
+.lm { grid-area: 2 / 1 / 3 / 2; }
+
+.lb { grid-area: 3 / 1 / 4 / 2; 
+transform: translateY(-100px);}
+
+.mb { grid-area: 3 / 2 / 4 / 3; }
+
+.mm { grid-area: 2 / 2 / 3 / 3; }
+
+.mt { grid-area: 1 / 2 / 2 / 3; }
+
+.rt { grid-area: 1 / 3 / 2 / 4; 
+font-size: 3vw; 
+transform: translateX(-60px);}
+
+.rm { grid-area: 2 / 3 / 3 / 4; }
+
+.rb { grid-area: 3 / 3 / 4 / 4; }
+
 
 </style>
